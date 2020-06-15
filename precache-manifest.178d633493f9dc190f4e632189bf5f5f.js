082bf90c188d48bc5db1ded556d42fa1b1c974b0
Dmitry Shapoval <almazmusic@gmail.com>
2020-06-16 00:56:35 +0300

    Updates

---
+++ precache-manifest.178d633493f9dc190f4e632189bf5f5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66506390c087d1bdd1ee3e4fe68e206a",
+    "url": "/weather-test/index.html"
+  },
+  {
+    "revision": "fb4fb8de906fb1c4f8d6",
+    "url": "/weather-test/static/css/main.da5a82b1.chunk.css"
+  },
+  {
+    "revision": "436aa84a86e277d2ea9c",
+    "url": "/weather-test/static/js/2.a2bd8c20.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/weather-test/static/js/2.a2bd8c20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb4fb8de906fb1c4f8d6",
+    "url": "/weather-test/static/js/main.58a4dba4.chunk.js"
+  },
+  {
+    "revision": "bb7be17c3480bfbc1928",
+    "url": "/weather-test/static/js/runtime-main.d684a103.js"
+  }
+]);
